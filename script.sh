622fd930320d5177272597c9aae5f06b61d4fe90
Jaeho Lee <me@jaeholee.org>
2017-04-05 16:48:06 +0900

    Cd to src dir

--- script.sh
+++ script.sh
@@ -5,6 +5,7 @@ set -ex
 # TODO This is the "test phase", tweak it as you see fit
 main() {
     #cross build --target $TARGET
+    cd svgcleaner/
     cross build --target $TARGET --release
 
     if [ ! -z $DISABLE_TESTS ]; then
